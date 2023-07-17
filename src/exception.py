b4d72ce94122aad32cad9ec02fa04fe27a3a1a8c
TamerZah <tamer.zahran@gmail.com>
2023-07-17 11:25:36 +0300

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # give detials about exception as in which line number
@@ -16,4 +17,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
